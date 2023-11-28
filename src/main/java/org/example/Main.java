977395e41a393436186ebf45cd8ae80837a15f13
rouvsen <rouvsen@gmail.com>
2023-11-28 14:47:10 +0400

    Add TwoSum Example V2 with O(n) to the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,14 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] res = TwoSumExample.handleTwoSum(new int[]{2,5,5,11}, 10);
-        System.out.println(Arrays.toString(res));
+
+//        int[] res1 = TwoSumExample.handleTwoSum_v1(new int[]{2,5,5,11}, 10);
+//        System.out.println(Arrays.toString(res1));
+//
+//        int[] res2 = TwoSumExample.handleTwoSum_v2(new int[]{2,5,5,11}, 10);
+//        System.out.println(Arrays.toString(res2));
+
+
+
     }
 }
--- src/main/java/org/example/TwoSumExample.java
+++ src/main/java/org/example/TwoSumExample.java
@@ -1,14 +1,11 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSumExample {
 
-    /*
-    Input: nums = [2,7,11,15], target = 9
-    Output: [0,1]
-    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-    */
-
-    public static int[] handleTwoSum(int[] nums, int target) {
+    public static int[] handleTwoSum_v1(int[] nums, int target) {
         int[] result = {-1, -1};
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -20,4 +17,23 @@ public class TwoSumExample {
         }
         return result;
     }
+
+    public static int[] handleTwoSum_v2(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
+            }
+        }
+
+        return new int[]{};
+    }
+
 }
