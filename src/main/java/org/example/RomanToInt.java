d783398aa024ce4f80439338527f80ac94cf811f
rouvsen <rouvsen@gmail.com>
2023-12-04 01:41:10 +0400

    Add RomanToInteger Algorithm to leetcode-solutions repo.

---
+++ src/main/java/org/example/RomanToInt.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInt {
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> romans = new HashMap<>();
+
+//        Input: s = "III"
+//        Output: 3
+//        Explanation: III = 3.
+//
+//        Input: s = "LVIII"
+//        Output: 58
+//        Explanation: L = 50, V= 5, III = 3.
+//
+//        Input: s = "MCMXCIV"
+//        Output: 1994
+//        Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+        romans.put('I', 1);
+        romans.put('V', 5);
+        romans.put('X', 10);
+        romans.put('L', 50);
+        romans.put('C', 100);
+        romans.put('D', 500);
+        romans.put('M', 1000);
+
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && romans.get(s.charAt(i)) < romans.get(s.charAt(i + 1))) {
+                answer -= romans.get(s.charAt(i));
+            } else {
+                answer += romans.get(s.charAt(i));
+            }
+        }
+
+        return answer;
+    }
+
+}
