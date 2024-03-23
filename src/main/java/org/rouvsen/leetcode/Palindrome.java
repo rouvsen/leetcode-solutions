282187c5f04dcec1313fcbe00c46930426df9d62
rouvsen <rouvsen@gmail.com>
2024-03-23 20:58:18 +0400

    add palindrome > best version

--- src/main/java/org/rouvsen/leetcode/Palindrome.java
+++ src/main/java/org/rouvsen/leetcode/Palindrome.java
@@ -5,9 +5,29 @@ import java.util.List;
 
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println("Is palindrome: " + isPalindrome(121));;
+//        System.out.println("Is palindrome: " + isPalindromeBasic(121));
+        System.out.println("Is palindrome: " + isPalindrome(121));
     }
+
     public static boolean isPalindrome(int num) {
+        if (num < 0) {
+            return false;
+        } else if(num >=0 && num < 10) {
+            return true;
+        } else {
+            int reverse = 0;
+            int temp = num;
+            while(temp != 0) {
+                reverse = reverse * 10 + temp % 10;
+                temp /= 10;
+            }
+            if(reverse == num)
+                return true;
+            return false;
+        }
+    }
+
+    public static boolean isPalindromeBasic(int num) {
         if(num >=0 && num < 10) {
             return true;
         }
